a7394ff27960a9529bbf889512d46edcf61bb037
Aver <kuba.grych@gmail.com>
2024-04-21 00:11:27 +0200

    handleChange, however it's not optimized, to-add: debouncing, cookie-backup

--- server/server.js
+++ server/server.js
@@ -160,14 +160,11 @@ app.post('/deltask', async(req, res)=>{
 
 app.post('/taskupdate', async(req, res)=>{
     try{
-        console.log("deltask edit received!");
         const {taskId, title, description} = req.body;
         const updatedTask = await Task.findOneAndUpdate(
             { taskId: taskId }, // Filter for finding the document
             { $set: { title: title, description: description } }, // Update operation
         );
-        console.log(title);
-        console.log(description);
 
         if (!updatedTask) {
             return res.status(404).json({ error: 'Task not found' });
